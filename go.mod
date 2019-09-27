2098ad653cdc5f7bcc774e665094d3f056829044
Phu D. Nguyen <Phu.Nguyen@sandata.com>
2019-09-27 16:17:04 +0700

    refactor worker

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/google/uuid v1.1.1
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/ndphu/swd-commons v0.0.0-20190927083510-d9b482248c56
 	golang.org/x/net v0.0.0-20190923162816-aa69164e4478 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- monitor/Monitor.go
+++ monitor/Monitor.go
@@ -7,10 +7,11 @@ import (
 	"github.com/eclipse/paho.mqtt.golang"
 	"github.com/globalsign/mgo/bson"
 	"github.com/google/uuid"
+	"github.com/ndphu/swd-commons/model"
+	"image"
 	"log"
 	"monitor-service/config"
 	"monitor-service/db"
-	"monitor-service/model"
 	"sync"
 	"time"
 )
@@ -21,21 +22,21 @@ func Start() {
 	doMonitor()
 	ticker := time.NewTicker(30 * time.Second)
 	for {
-		log.Println("[Monitor] Waiting for ticker...")
+		log.Println("[MONITOR] Waiting for ticker...")
 		startAt := <-ticker.C
-		log.Println("[Monitor] Job start at", startAt)
+		log.Println("[MONITOR] Job start at", startAt)
 		doMonitor()
-		log.Println("[Monitor] Job done at", time.Now())
+		log.Println("[MONITOR] Job done at", time.Now())
 	}
 }
 
 func doMonitor() {
 	rules := make([]model.Rule, 0)
 	if err := dao.Collection("rule").Find(nil).All(&rules); err != nil {
-		log.Println("[Monitor] Fail to load rules by error:", err.Error())
+		log.Println("[MONITOR] Fail to load rules by error:", err.Error())
 		return
 	}
-	log.Println("[Monitor] Working on", len(rules), "rules")
+	log.Println("[MONITOR] Working on", len(rules), "rules")
 
 	wg := sync.WaitGroup{}
 
@@ -52,7 +53,7 @@ func doMonitor() {
 }
 
 func processRule(r model.Rule) {
-	log.Println("[Monitor] Processing rule", r.Id.Hex())
+	log.Println("[MONITOR] Processing rule", r.Id.Hex())
 
 	frameDelay := 1000 //ms
 	totalPics := 5
@@ -86,7 +87,7 @@ func processRule(r model.Rule) {
 
 	captureClient := mqtt.NewClient(opts)
 	if token := captureClient.Connect(); token.Wait() && token.Error() != nil {
-		log.Println("[Monitor] Fail to connect to MQTT", token.Error())
+		log.Println("[MONITOR] Fail to connect to MQTT", token.Error())
 		return
 	}
 
@@ -96,23 +97,23 @@ func processRule(r model.Rule) {
 
 	select {
 	case <-timeout.C:
-		log.Println("[Monitor] Capture timeout.")
+		log.Println("[MONITOR] Capture timeout.")
 		if err := saveCaptureFailEvent(r); err != nil {
-			log.Println("[Monitor] Fail to save event by error", err.Error())
+			log.Println("[MONITOR] Fail to save event by error", err.Error())
 		} else {
-			log.Println("[Monitor] Save event successfully")
+			log.Println("[MONITOR] Save event successfully")
 		}
 		return
 	case <-done:
-		log.Println("[Monitor] Capture completed. Frame count:", len(pics))
+		log.Println("[MONITOR] Capture completed. Frame count:", len(pics))
 		break
 	}
 
-	if response, err := recognizeBulk(pics); err != nil {
-		log.Println("[Monitor] Fail to recognize bulk by error:", err.Error())
+	if response, err := recognizeBulk(r.ProjectId, pics); err != nil {
+		log.Println("[MONITOR] Fail to recognize bulk by error:", err.Error())
 		saveRecognizeFailEvent(r, err)
 	} else {
-		log.Println("[Monitor] Device:", r.DeviceId, "Found labels:", response.Labels)
+		log.Println("[MONITOR] Device:", r.DeviceId, "Found labels:", response.Labels)
 
 		saveRecognizeSuccessEvent(r, response.Labels)
 
@@ -123,19 +124,19 @@ func processRule(r model.Rule) {
 
 		matchPeople := false
 		for _, label := range response.Labels {
-			if label == r.Label {
+			if label == r.ProjectId {
 				matchPeople = true
 				break
 			}
 		}
 
-		log.Println("[Monitor]", "Match People:", matchPeople)
+		log.Println("[MONITOR]", "Match People:", matchPeople)
 
 		if matchPeople {
 			if events, err := getRecentRecognizeResult(r); err != nil {
-				log.Println("[Monitor]", "Fail to get recent events by error", err.Error())
+				log.Println("[MONITOR]", "Fail to get recent events by error", err.Error())
 			} else {
-				log.Println("[Monitor]", "Found", len(events), "events")
+				log.Println("[MONITOR]", "Found", len(events), "events")
 				shouldSendNotification := true
 
 				consecutiveMissing := 0
@@ -143,7 +144,7 @@ func processRule(r model.Rule) {
 				for _, event := range events {
 					hasLabel := false
 					for _, l := range event.Labels {
-						if l == r.Label {
+						if l == r.ProjectId {
 							hasLabel = true
 							break
 						}
@@ -162,45 +163,44 @@ func processRule(r model.Rule) {
 				}
 				// TODO: should check last present after consecutive too
 
-				log.Println("[Monitor]", "Rule:", r.Id, "Should send notification:", shouldSendNotification)
+				log.Println("[MONITOR]", "Rule:", r.Id, "Should send notification:", shouldSendNotification)
 				if shouldSendNotification {
 					sendNotification(r)
 				}
 			}
 		}
 
-
 		//if matchPeople {
 		//	trackingMap[r.DeviceId].FirstMissing = time.Unix(0, 0)
 		//	if trackingMap[r.DeviceId].FirstSeen.IsZero() {
-		//		log.Println("[Monitor]", "First see user camera at", time.Now())
+		//		log.Println("[MONITOR]", "First see user camera at", time.Now())
 		//		trackingMap[r.DeviceId].FirstSeen = time.Now()
 		//	} else {
 		//		since := time.Since(trackingMap[r.DeviceId].FirstSeen)
 		//		if since.Minutes() > float64(r.Interval) {
 		//			sendNotification(r)
 		//		} else {
-		//			log.Println("[Monitor]", "User is on camera for", since.Seconds(), "seconds")
+		//			log.Println("[MONITOR]", "User is on camera for", since.Seconds(), "seconds")
 		//
 		//		}
 		//	}
 		//} else {
 		//	if trackingMap[r.DeviceId].FirstMissing.IsZero() {
-		//		log.Println("[Monitor]", "First seen user MISSING at", time.Now())
+		//		log.Println("[MONITOR]", "First seen user MISSING at", time.Now())
 		//		trackingMap[r.DeviceId].FirstMissing = time.Now()
 		//	} else {
 		//		since := time.Since(trackingMap[r.DeviceId].FirstMissing)
 		//		if since.Seconds() > 45.0 {
-		//			log.Println("[Monitor]", "User may leaving his desk. Reset tracking.")
+		//			log.Println("[MONITOR]", "User may leaving his desk. Reset tracking.")
 		//			trackingMap[r.DeviceId] = nil
 		//		} else {
-		//			log.Println("[Monitor]", "User still MISSING for ", since.Seconds(), "seconds")
+		//			log.Println("[MONITOR]", "User still MISSING for ", since.Seconds(), "seconds")
 		//		}
 		//	}
 		//}
 	}
 
-	log.Println("[Monitor] Processing done for rule", r.Id.Hex())
+	log.Println("[MONITOR] Processing done for rule", r.Id.Hex())
 }
 
 func sendNotification(rule model.Rule) {
@@ -215,27 +215,27 @@ func sendNotification(rule model.Rule) {
 	}
 
 	if err := dao.Collection("notification").Insert(nf); err != nil {
-		log.Println("[Monitor]", "Fail to persist notification by error:", err.Error())
+		log.Println("[MONITOR]", "Fail to persist notification by error:", err.Error())
 	} else {
-		log.Println("[Monitor]", "Notification persisted successfully")
+		log.Println("[MONITOR]", "Notification persisted successfully")
 	}
 
 	if payload, err := json.Marshal(nf); err != nil {
-		log.Println("[Monitor]", "Fail to marshal notification")
+		log.Println("[MONITOR]", "Fail to marshal notification")
 	} else {
 		topic := fmt.Sprintf("/3ml/project/%s/notification", rule.ProjectId)
-		log.Println("[Monitor]", "Publishing notification to topic", topic)
+		log.Println("[MONITOR]", "Publishing notification to topic", topic)
 		clientId := uuid.New().String()
 		opts := GetDefaultOps(clientId)
 		client := mqtt.NewClient(opts)
 		if token := client.Connect(); token.Wait() && token.Error() != nil {
-			log.Println("[Monitor]", "Fail to connect to MQTT", token.Error())
+			log.Println("[MONITOR]", "Fail to connect to MQTT", token.Error())
 			return
 		}
 		if token := client.Publish(topic, 0, false, payload); token.Wait() && token.Error() != nil {
-			log.Println("[Monitor]", "Fail publish notification via MQTT", token.Error())
+			log.Println("[MONITOR]", "Fail publish notification via MQTT", token.Error())
 		} else {
-			log.Println("[Monitor]", "Notification published successfully")
+			log.Println("[MONITOR]", "Notification published successfully")
 		}
 
 		defer client.Disconnect(200)
@@ -293,7 +293,7 @@ func getFrameOutTopic(deviceId string) string {
 	return "/3ml/device/" + deviceId + "/framed/out"
 }
 
-func recognizeBulk(images [][]byte) (*BulkRecognizeResponse, error) {
+func recognizeBulk(projectId string, images [][]byte) (*BulkRecognizeResponse, error) {
 	reqId := uuid.New().String()
 	rpcReqPayload, _ := json.Marshal(BulkRecognizeRequest{
 		RequestId: reqId,
@@ -301,8 +301,9 @@ func recognizeBulk(images [][]byte) (*BulkRecognizeResponse, error) {
 	})
 
 	rpcResponse := make(chan BulkRecognizeResponse)
+
 	rpcRequestTopic := "/3ml/rpc/recognizeFacesBulk/request"
-	rpcResponseTopic := "/3ml/rpc/recognizeFacesBulk/response/" + reqId
+	rpcResponseTopic := "/3ml/rpc/" + "/response/" + reqId
 
 	clientId := uuid.New().String()
 
@@ -339,13 +340,21 @@ func recognizeBulk(images [][]byte) (*BulkRecognizeResponse, error) {
 }
 
 type BulkRecognizeRequest struct {
-	Images    [][]byte `json:"payload"`
-	RequestId string   `json:"requestId"`
+	Images              [][]byte `json:"payload"`
+	IncludeFacesDetails bool     `json:"includeFacesDetails"`
+	RequestId           string   `json:"requestId"`
+	ResponseTo          string   `json:"responseTo"`
 }
 
 type BulkRecognizeResponse struct {
-	Labels []string `json:"labels"`
-	Error  error    `json:"error"`
+	Labels          []string      `json:"labels"`
+	FaceDetailsList []FaceDetails `json:"faceDetailsList"`
+	Error           error         `json:"error"`
+}
+
+type FaceDetails struct {
+	Rect       image.Rectangle `json:"rect"`
+	Descriptor []float32       `json:"descriptor"`
 }
 
 type DetectionTracking struct {
